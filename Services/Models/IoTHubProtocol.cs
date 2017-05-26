2e88f550356dd63a2b9ab54d503f837645f72b80
Devis Lucato <devis@lucato.it>
2017-05-26 13:01:20 -0700

    Add device types configuration and endpoint
    This adds a new endpoint to list all the supported device types. Device
    types are configured via JSON files external to the assembly, so that
    users can add custom types without rebuilding the service. The files
    include a README describing the format of these files.
    I was going to use HOCON for these files too, for a cleaner look, but
    I'm actually reconsidering that approach and the use of Newtonsoft
    greatly reduces the code required to parse these files (at least in
    .NET).

---
+++ Services/Models/IoTHubProtocol.cs
@@ -0,0 +1,11 @@
+// Copyright (c) Microsoft. All rights reserved.
+
+namespace Microsoft.Azure.IoTSolutions.DeviceSimulation.Services.Models
+{
+    public enum IoTHubProtocol
+    {
+        AMQP = 1,
+        MQTT = 2,
+        HTTP = 3
+    }
+}
