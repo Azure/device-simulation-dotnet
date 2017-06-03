44e7261becf4de115f15a99bbd365df653152013
Devis Lucato <devis@lucato.it>
2017-06-02 18:37:18 -0700

    Remove Akka HOCON from web service, use app.confg for settings - use HOCON only when using Akka

---
+++ SimulationAgent/Runtime/ConfigData.cs
@@ -0,0 +1,51 @@
+// Copyright (c) Microsoft. All rights reserved.
+
+using System;
+using System.Collections;
+using System.Configuration;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+// TODO: tests
+namespace Microsoft.Azure.IoTSolutions.DeviceSimulation.SimulationAgent.Runtime
+{
+    public interface IConfigData
+    {
+        string GetString(string key);
+        int GetInt(string key);
+    }
+
+    public class ConfigData : IConfigData
+    {
+        public string GetString(string key)
+        {
+            var value = ConfigurationManager.AppSettings[key];
+            return ReplaceEnvironmentVariables(value);
+        }
+
+        public int GetInt(string key)
+        {
+            return Convert.ToInt32(this.GetString(key));
+        }
+
+        private static string ReplaceEnvironmentVariables(string value)
+        {
+            // Extract the name of all the substitutions required
+            // using the following pattern, e.g. ${VAR_NAME}
+            const string pattern = @"\${(?'key'[a-zA-Z_][a-zA-Z0-9_]*)}";
+            var keys = (from Match m
+                        in Regex.Matches(value, pattern)
+                        select m.Groups[1].Value).ToArray();
+
+            foreach (DictionaryEntry x in Environment.GetEnvironmentVariables())
+            {
+                if (keys.Contains(x.Key))
+                {
+                    value = value.Replace("${" + x.Key + "}", x.Value.ToString());
+                }
+            }
+
+            return value;
+        }
+    }
+}
