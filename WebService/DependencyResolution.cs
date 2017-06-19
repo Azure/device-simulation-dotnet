7b7eeaefeaee9df71f44ef5fbc6ffb48075d99c9
Devis Lucato <devis@lucato.it>
2017-06-18 18:02:29 -0700

    upgrade dependency injection

--- WebService/DependencyResolution.cs
+++ WebService/DependencyResolution.cs
@@ -1,107 +1,41 @@
 // Copyright (c) Microsoft. All rights reserved.
 
 using System.Reflection;
-using System.Web.Http;
 using Autofac;
-using Autofac.Integration.WebApi;
+using Autofac.Extensions.DependencyInjection;
 using Microsoft.Azure.IoTSolutions.DeviceSimulation.Services;
 using Microsoft.Azure.IoTSolutions.DeviceSimulation.Services.Runtime;
 using Microsoft.Azure.IoTSolutions.DeviceSimulation.WebService.Runtime;
-using Owin;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace Microsoft.Azure.IoTSolutions.DeviceSimulation.WebService
 {
     public class DependencyResolution
     {
-        //// <summary>
-        /// Provide factory pattern for dependencies that are instantiated
-        /// multiple times during the application lifetime.
-        /// How to use:
-        ///
-        /// class MyClass : IMyClass {
-        ///     public MyClass(DependencyInjection.IFactory factory) {
-        ///         this.factory = factory;
-        ///     }
-        ///     public SomeMethod() {
-        ///         var instance1 = this.factory.Resolve<ISomething>();
-        ///         var instance2 = this.factory.Resolve<ISomething>();
-        ///         var instance3 = this.factory.Resolve<ISomething>();
-        ///     }
-        /// }
-        /// </summary>
-        public interface IFactory
-        {
-            T Resolve<T>();
-        }
-
-        public class Factory : IFactory
-        {
-            private static IContainer container;
-
-            public static void RegisterContainer(IContainer c)
-            {
-                container = c;
-            }
-
-            public T Resolve<T>()
-            {
-                return container.Resolve<T>();
-            }
-        }
-
         /// <summary>
         /// Autofac configuration. Find more information here:
-        /// http://docs.autofac.org/en/latest/integration/owin.html
-        /// http://autofac.readthedocs.io/en/latest/register/scanning.html
+        /// @see http://docs.autofac.org/en/latest/integration/aspnetcore.html
         /// </summary>
-        public static IContainer Setup(IAppBuilder app, HttpConfiguration config)
+        public static IContainer Setup(IServiceCollection services)
         {
             var builder = new ContainerBuilder();
 
+            builder.Populate(services);
+
             AutowireAssemblies(builder);
             SetupCustomRules(builder);
-            SetupWebApiRules(builder);
 
             var container = builder.Build();
-
-            Factory.RegisterContainer(container);
-
-            SetupWebApiResolution(app, config, container);
+            RegisterFactory(container);
 
             return container;
         }
 
-        /// <summary>
-        /// Autowire interfaces to classes. Note that the solution assemblies
-        /// are explicitly managed here. This could be extended to analyze
-        /// all the assemblies directly and indirectly referenced.
-        /// </summary>
+        /// <summary>Autowire interfaces to classes from all the assemblies</summary>
         private static void AutowireAssemblies(ContainerBuilder builder)
         {
-            var assembly = Assembly.GetExecutingAssembly();
+            var assembly = Assembly.GetEntryAssembly();
             builder.RegisterAssemblyTypes(assembly).AsImplementedInterfaces();
-
-            assembly = typeof(ServicesConfig).Assembly;
-            builder.RegisterAssemblyTypes(assembly).AsImplementedInterfaces();
-        }
-
-        private static void SetupWebApiRules(ContainerBuilder builder)
-        {
-            // Register Web API controllers in executing assembly.
-            builder.RegisterApiControllers(Assembly.GetExecutingAssembly());
-        }
-
-        private static void SetupWebApiResolution(IAppBuilder app, HttpConfiguration config, IContainer container)
-        {
-            // Create and assign a dependency resolver for Web API to use.
-            config.DependencyResolver = new AutofacWebApiDependencyResolver(container);
-
-            // The Autofac middleware should be the first middleware added to
-            // the IAppBuilder.
-            app.UseAutofacMiddleware(container);
-
-            // Make sure the Autofac lifetime scope is passed to Web API.
-            app.UseAutofacWebApi(config);
         }
 
         /// <summary>Setup Custom rules overriding autowired ones.</summary>
@@ -121,6 +55,48 @@ namespace Microsoft.Azure.IoTSolutions.DeviceSimulation.WebService
             // TODO: revisit when migrating to ASP.NET Core.
             builder.RegisterType<Simulations>().As<ISimulations>().SingleInstance();
             builder.RegisterType<DeviceTypes>().As<IDeviceTypes>().SingleInstance();
+        }
+
+        private static void RegisterFactory(IContainer container)
+        {
+            Factory.RegisterContainer(container);
+        }
+
+        /// <summary>
+        /// Provide factory pattern for dependencies that are instantiated
+        /// multiple times during the application lifetime.
+        /// How to use:
+        /// <code>
+        /// class MyClass : IMyClass {
+        ///     public MyClass(DependencyInjection.IFactory factory) {
+        ///         this.factory = factory;
+        ///     }
+        ///     public SomeMethod() {
+        ///         var instance1 = this.factory.Resolve<ISomething>();
+        ///         var instance2 = this.factory.Resolve<ISomething>();
+        ///         var instance3 = this.factory.Resolve<ISomething>();
+        ///     }
+        /// }
+        /// </code>
+        /// </summary>
+        public interface IFactory
+        {
+            T Resolve<T>();
+        }
+
+        public class Factory : IFactory
+        {
+            private static IContainer container;
+
+            public static void RegisterContainer(IContainer c)
+            {
+                container = c;
+            }
+
+            public T Resolve<T>()
+            {
+                return container.Resolve<T>();
+            }
         }
     }
 }
