b157ff853e90170bbdd952a1c6952ffd6cdf33d1
Devis Lucato <devis@lucato.it>
2017-06-27 10:51:26 -0700

    Misc

---
+++ Services/Exceptions/ExternalDependencyException.cs
@@ -0,0 +1,31 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+using System;
+
+namespace Microsoft.Azure.IoTSolutions.DeviceSimulation.Services.Exceptions
+{
+    /// <summary>
+    /// This exception is thrown when the service failed to communicate with
+    /// an external dependency, either because the dependency is unavailable
+    /// or because it is failing with a 'retriable' error (e.g. timeout).
+    ///
+    /// When used for HTTP requests, a request error is retriable if:
+    /// * the request timed out, or
+    /// * the status code is one of 404, 408, 429
+    /// </summary>
+    public class ExternalDependencyException : Exception
+    {
+        public ExternalDependencyException() : base()
+        {
+        }
+
+        public ExternalDependencyException(string message) : base(message)
+        {
+        }
+
+        public ExternalDependencyException(string message, Exception innerException)
+            : base(message, innerException)
+        {
+        }
+    }
+}
