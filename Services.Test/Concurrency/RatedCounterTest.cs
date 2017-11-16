d3f55a76c8837b50c841fd3a01929446de1e3c08
Devis Lucato <devis@lucato.it>
2017-11-15 17:15:15 -0800

    Fix #122 + backport fix for flaky test

---
+++ Services.Test/Concurrency/RatedCounterTest.cs
@@ -0,0 +1,41 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+using Microsoft.Azure.IoTSolutions.DeviceSimulation.Services.Concurrency;
+using Microsoft.Azure.IoTSolutions.DeviceSimulation.Services.Diagnostics;
+using Microsoft.Azure.IoTSolutions.DeviceSimulation.Services.Exceptions;
+using Moq;
+using Services.Test.helpers;
+using Xunit;
+
+namespace Services.Test.Concurrency
+{
+    public class RatedCounterTest
+    {
+        /**
+         * Checks the configuration validation
+         * Also covers https://github.com/Azure/device-simulation-dotnet/issues/122
+         */
+        [Fact, Trait(Constants.TYPE, Constants.UNIT_TEST)]
+        public void ItDoesntAllowLowCounterRate()
+        {
+            Assert.Throws<InvalidConfigurationException>(() => new BadCounter1(new Mock<ILogger>().Object));
+            Assert.Throws<InvalidConfigurationException>(() => new BadCounter2(new Mock<ILogger>().Object));
+        }
+
+        class BadCounter1 : RatedCounter
+        {
+            public BadCounter1(ILogger logger)
+                : base(1, 999, "BadCounter1", logger)
+            {
+            }
+        }
+
+        class BadCounter2 : RatedCounter
+        {
+            public BadCounter2(ILogger logger)
+                : base(0, 99999, "BadCounter2", logger)
+            {
+            }
+        }
+    }
+}
