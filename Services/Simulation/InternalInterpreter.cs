1034ea609bda4f10006ea2f5d109d93448838dd4
Devis Lucato <devis@lucato.it>
2017-11-30 17:29:25 -0800

    Add internal scripts

---
+++ Services/Simulation/InternalInterpreter.cs
@@ -0,0 +1,192 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+using System;
+using System.Collections.Generic;
+using Microsoft.Azure.IoTSolutions.DeviceSimulation.Services.Diagnostics;
+using Newtonsoft.Json;
+
+namespace Microsoft.Azure.IoTSolutions.DeviceSimulation.Services.Simulation
+{
+    public interface IInternalInterpreter
+    {
+        // List of supported scripts, used for input validation
+        HashSet<string> SupportedScripts { get; }
+
+        /// <summary>Invoke one of the internal scripts</summary>
+        /// <param name="scriptPath">Name of the script</param>
+        /// <param name="scriptParams">Script parameters, e.g. min, max, step</param>
+        /// <param name="context">Context, e.g. current time, device Id, device Model</param>
+        /// <param name="state">Current device sensors state</param>
+        /// <returns>New device sensors state</returns>
+        Dictionary<string, object> Invoke(
+            string scriptPath, object scriptParams,
+            Dictionary<string, object> context,
+            Dictionary<string, object> state);
+    }
+
+    public class InternalInterpreter : IInternalInterpreter
+    {
+        private const string SCRIPT_RANDOM = "math.random.withinrange";
+        private const string SCRIPT_INCREASING = "math.increasing";
+        private const string SCRIPT_DECREASING = "math.decreasing";
+        private const double EQUALITY_PRECISION = .001;
+
+        public HashSet<string> SupportedScripts => new HashSet<string>
+        {
+            SCRIPT_RANDOM,
+            SCRIPT_INCREASING,
+            SCRIPT_DECREASING
+        };
+
+        private readonly Random random;
+        private readonly ILogger log;
+
+        public InternalInterpreter(ILogger logger)
+        {
+            this.log = logger;
+            this.random = new Random();
+        }
+
+        public Dictionary<string, object> Invoke(
+            string scriptPath,
+            object scriptParams,
+            Dictionary<string, object> context,
+            Dictionary<string, object> state)
+        {
+            switch (scriptPath.ToLowerInvariant())
+            {
+                case SCRIPT_RANDOM:
+                    return this.RunRandomNumberScript(scriptParams, state);
+
+                case SCRIPT_INCREASING:
+                    return this.RunIncreasingScript(scriptParams, state);
+
+                case SCRIPT_DECREASING:
+                    return this.RunDecreasingScript(scriptParams, state);
+
+                default:
+                    throw new NotSupportedException($"Unknown script `{scriptPath}`.");
+            }
+        }
+
+        // For each sensors specified, generate a random number in the range requested
+        private Dictionary<string, object> RunRandomNumberScript(object scriptParams, Dictionary<string, object> state)
+        {
+            var sensors = this.JsonParamAsDictionary(scriptParams);
+            foreach (var sensor in sensors)
+            {
+                (double min, double max) = this.GetMinMaxParameters(sensor.Value);
+                state[sensor.Key] = this.random.NextDouble() * (max - min) + min;
+            }
+
+            return state;
+        }
+
+        // For each sensors specified, increase the current state, up to a maximum, then restart from a minimum
+        private Dictionary<string, object> RunIncreasingScript(object scriptParams, Dictionary<string, object> state)
+        {
+            var sensors = this.JsonParamAsDictionary(scriptParams);
+            foreach (var sensor in sensors)
+            {
+                // Extract scripts parameters from the device model script configuration
+                (double min, double max, double step) = this.GetMinMaxStepParameters(sensor.Value);
+
+                // Add the sensor to the state if missing
+                if (!state.ContainsKey(sensor.Key))
+                {
+                    state[sensor.Key] = min;
+                }
+
+                double current = Convert.ToDouble(state[sensor.Key]);
+                double next = Math.Abs(current - max) < EQUALITY_PRECISION ? min : Math.Min(current + step, max);
+
+                state[sensor.Key] = next;
+            }
+
+            return state;
+        }
+
+        // For each sensors specified, increase the current state, up to a maximum, then restart from a minimum
+        private Dictionary<string, object> RunDecreasingScript(object scriptParams, Dictionary<string, object> state)
+        {
+            var sensors = this.JsonParamAsDictionary(scriptParams);
+            foreach (var sensor in sensors)
+            {
+                // Extract scripts parameters from the device model script configuration
+                (double min, double max, double step) = this.GetMinMaxStepParameters(sensor.Value);
+
+                // Add the sensor to the state if missing
+                if (!state.ContainsKey(sensor.Key))
+                {
+                    state[sensor.Key] = max;
+                }
+
+                double current = Convert.ToDouble(state[sensor.Key]);
+                double next = Math.Abs(current - max) < EQUALITY_PRECISION ? max : Math.Max(current - step, min);
+
+                state[sensor.Key] = next;
+            }
+
+            return state;
+        }
+
+        private (double, double) GetMinMaxParameters(object parameters)
+        {
+            var dict = this.JsonParamAsDictionary(parameters);
+
+            if (!dict.ContainsKey("Min"))
+            {
+                this.log.Error("Missing 'Min' parameter", () => new { dict });
+                throw new FormatException("Missing 'Min' parameter");
+            }
+
+            if (!dict.ContainsKey("Max"))
+            {
+                this.log.Error("Missing 'Max' parameter", () => new { dict });
+                throw new FormatException("Missing 'Max' parameter");
+            }
+
+            return (Convert.ToDouble(dict["Min"]), Convert.ToDouble(dict["Max"]));
+        }
+
+        private (double, double, double) GetMinMaxStepParameters(object parameters)
+        {
+            var dict = this.JsonParamAsDictionary(parameters);
+
+            if (!dict.ContainsKey("Min"))
+            {
+                this.log.Error("Missing 'Min' parameter", () => new { dict });
+                throw new FormatException("Missing 'Min' parameter");
+            }
+
+            if (!dict.ContainsKey("Max"))
+            {
+                this.log.Error("Missing 'Max' parameter", () => new { dict });
+                throw new FormatException("Missing 'Max' parameter");
+            }
+
+            if (!dict.ContainsKey("Step"))
+            {
+                this.log.Error("Missing 'Step' parameter", () => new { dict });
+                throw new FormatException("Missing 'Step' parameter");
+            }
+
+            return (Convert.ToDouble(dict["Min"]),
+                Convert.ToDouble(dict["Max"]),
+                Convert.ToDouble(dict["Step"]));
+        }
+
+        private Dictionary<string, object> JsonParamAsDictionary(object parameters)
+        {
+            try
+            {
+                return JsonConvert.DeserializeObject<Dictionary<string, object>>(JsonConvert.SerializeObject(parameters));
+            }
+            catch (Exception e)
+            {
+                this.log.Error("Unknown script parameters format. The parameters should be passed key-value dictionary.", () => new { parameters, e });
+                throw new NotSupportedException("Unknown script parameters format. The parameters should be passed key-value dictionary.");
+            }
+        }
+    }
+}
--- Services/Simulation/ScriptInterpreter.cs
+++ Services/Simulation/ScriptInterpreter.cs
@@ -18,13 +18,16 @@ namespace Microsoft.Azure.IoTSolutions.DeviceSimulation.Services.Simulation
     public class ScriptInterpreter : IScriptInterpreter
     {
         private readonly IJavascriptInterpreter jsInterpreter;
+        private readonly IInternalInterpreter intInterpreter;
         private readonly ILogger log;
 
         public ScriptInterpreter(
             IJavascriptInterpreter jsInterpreter,
+            IInternalInterpreter intInterpreter,
             ILogger logger)
         {
             this.jsInterpreter = jsInterpreter;
+            this.intInterpreter = intInterpreter;
             this.log = logger;
         }
 
@@ -37,13 +40,19 @@ namespace Microsoft.Azure.IoTSolutions.DeviceSimulation.Services.Simulation
             {
                 default:
                     this.log.Error("Unknown script type", () => new { script.Type });
-                    throw new NotSupportedException($"Unknown script type `${script.Type}`.");
+                    throw new NotSupportedException($"Unknown script type `{script.Type}`.");
 
                 case "javascript":
                     this.log.Debug("Invoking JS", () => new { script.Path, context, state });
-                    var result = this.jsInterpreter.Invoke(script.Path, context, state);
-                    this.log.Debug("JS result", () => new { result });
-                    return result;
+                    var jsResult = this.jsInterpreter.Invoke(script.Path, context, state);
+                    this.log.Debug("JS result", () => new { result = jsResult });
+                    return jsResult;
+
+                case "internal":
+                    this.log.Debug("Invoking internal script", () => new { script.Path, context, state });
+                    var intResult = this.intInterpreter.Invoke(script.Path, script.Params, context, state);
+                    this.log.Debug("Internal script result", () => new { intresult = intResult });
+                    return intResult;
             }
         }
     }
