8042a201590fab25c710a60476e1620033ce7366
Jill Bender <jbend@microsoft.com>
2018-03-15 16:42:29 -0700

    Add skeleton DevicePropertiesActor and integrate with SimulationRunner

---
+++ SimulationAgent/DeviceProperties/IDevicePropertiesLogic.cs
@@ -0,0 +1,12 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+using Microsoft.Azure.IoTSolutions.DeviceSimulation.Services.Models;
+
+namespace Microsoft.Azure.IoTSolutions.DeviceSimulation.SimulationAgent.DeviceProperties
+{
+    public interface IDevicePropertiesLogic
+    {
+        void Setup(IDevicePropertiesActor devicePropertiesActor, string deviceId, DeviceModel deviceModel);
+        void Run();
+    }
+}
