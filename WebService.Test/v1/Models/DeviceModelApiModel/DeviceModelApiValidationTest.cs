c2a0bd59e6766ccff543462aefe8f4e6e25c37b7
REDMOND\v-huixio <maple10001@foxmail.com>
2018-06-14 11:37:46 -0700

    Remove unused usings

--- WebService.Test/v1/Models/DeviceModelApiModel/DeviceModelApiValidationTest.cs
+++ WebService.Test/v1/Models/DeviceModelApiModel/DeviceModelApiValidationTest.cs
@@ -2,13 +2,8 @@
 
 using System;
 using System.Collections.Generic;
-using Microsoft.Azure.IoTSolutions.DeviceSimulation.Services.Diagnostics;
-using Microsoft.Azure.IoTSolutions.DeviceSimulation.Services.Models;
-using Microsoft.Azure.IoTSolutions.DeviceSimulation.WebService.v1.Exceptions;
 using Microsoft.Azure.IoTSolutions.DeviceSimulation.WebService.v1.Models.DeviceModelApiModel;
-using Moq;
 using Newtonsoft.Json;
-using Newtonsoft.Json.Linq;
 using WebService.Test.helpers;
 using Xunit;
 
