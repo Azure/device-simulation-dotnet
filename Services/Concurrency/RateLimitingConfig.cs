4c004d205368c371c92fc13ca64d0fa4e0250478
Devis Lucato <dluc@users.noreply.github.com>
2018-08-07 20:17:40 -0700

    Support sending more than 2 billions messages per day (only up to ~9 quintillions, to be fixed)

--- Services/Concurrency/RateLimitingConfig.cs
+++ Services/Concurrency/RateLimitingConfig.cs
@@ -10,7 +10,7 @@ namespace Microsoft.Azure.IoTSolutions.DeviceSimulation.Services.Concurrency
         int TwinWritesPerSecond { get; }
         int ConnectionsPerSecond { get; }
         int DeviceMessagesPerSecond { get; }
-        int DeviceMessagesPerDay { get; }
+        long DeviceMessagesPerDay { get; }
     }
 
     public class RateLimitingConfig : IRateLimitingConfig
@@ -20,6 +20,6 @@ namespace Microsoft.Azure.IoTSolutions.DeviceSimulation.Services.Concurrency
         public int TwinWritesPerSecond { get; set; }
         public int ConnectionsPerSecond { get; set; }
         public int DeviceMessagesPerSecond { get; set; }
-        public int DeviceMessagesPerDay { get; set; }
+        public long DeviceMessagesPerDay { get; set; }
     }
 }
