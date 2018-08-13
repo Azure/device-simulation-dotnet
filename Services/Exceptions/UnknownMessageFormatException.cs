274079bf88da67d2116776b11a3694a0d19554f9
sasathy <39567983+sasathy@users.noreply.github.com>
2018-08-13 14:08:52 -0700

    Adding Protobuf support for truck (#235)
    * Changes to Support sending encrypted Protobuf messages in addition to Json

---
+++ Services/Exceptions/UnknownMessageFormatException.cs
@@ -0,0 +1,26 @@
+// Copyright (c) Microsoft. All rights reserved.
+
+using System;
+
+namespace Microsoft.Azure.IoTSolutions.DeviceSimulation.Services.Exceptions
+{
+    /// <summary>
+    /// This exception is thrown when a client is requesting a message format that
+    /// doesn't exist yet.
+    /// </summary>
+    public class UnknownMessageFormatException : Exception
+    {
+        public UnknownMessageFormatException() : base()
+        {
+        }
+
+        public UnknownMessageFormatException(string message) : base(message)
+        {
+        }
+
+        public UnknownMessageFormatException(string message, Exception innerException)
+            : base(message, innerException)
+        {
+        }
+    }
+}
