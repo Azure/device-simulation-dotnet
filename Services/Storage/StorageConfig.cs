fec6e4590d9347418630d51b38b50f2fed03aec7
peterfelts <peezee1978-gethub@yahoo.com>
2018-08-17 16:41:14 -0700

    Storage settings

---
+++ Services/Storage/StorageConfig.cs
@@ -0,0 +1,13 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+namespace Microsoft.Azure.IoTSolutions.DeviceSimulation.Services.Storage
+{
+    public class StorageConfig
+    {
+        public string StorageType { get; set; }
+        public string DocumentDbConnString { get; set; }
+        public string DocumentDbDatabase { get; set; }
+        public string DocumentDbCollection { get; set; }
+        public int DocumentDbRUs { get; set; }
+    }
+}
