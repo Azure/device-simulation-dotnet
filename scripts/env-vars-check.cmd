6597c47e07832a1a657ff134032a25d57cdfb090
Devis Lucato <dluc@users.noreply.github.com>
2018-09-16 18:43:12 -0700

    Add new settings for the storage account used to import devices

--- scripts/env-vars-check.cmd
+++ scripts/env-vars-check.cmd
@@ -7,6 +7,16 @@ IF "%PCS_IOTHUB_CONNSTRING%" == "" (
     exit /B 1
 )
 
+IF "%PCS_STORAGEADAPTER_DOCUMENTDB_CONNSTRING%" == "" (
+    echo Error: the PCS_STORAGEADAPTER_DOCUMENTDB_CONNSTRING environment variable is not defined.
+    exit /B 1
+)
+
+IF "%PCS_AZURE_STORAGE_ACCOUNT%" == "" (
+    echo Error: the PCS_AZURE_STORAGE_ACCOUNT environment variable is not defined.
+    exit /B 1
+)
+
 IF "%PCS_STORAGEADAPTER_WEBSERVICE_URL%" == "" (
     echo Error: the PCS_STORAGEADAPTER_WEBSERVICE_URL environment variable is not defined.
     exit /B 1
