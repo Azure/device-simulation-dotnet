17e614cec2a67f7a98744b01c07ca19f8e92836c
Devis Lucato <dluc@users.noreply.github.com>
2018-12-03 23:27:52 -0800

    Update dev script

--- scripts/development/start-storage-adapter.sh
+++ scripts/development/start-storage-adapter.sh
@@ -2,7 +2,12 @@
 
 PCS_STORAGEADAPTER_DOCUMENTDB_CONNSTRING="AccountEndpoint=https://_______.documents.azure.com:443/;AccountKey=_______;"
 
+if [ ! -z "$1" ]; then
+    PCS_STORAGEADAPTER_DOCUMENTDB_CONNSTRING="$1"
+fi
+
 docker run -itd \
     -p 9022:9022 \
     -e PCS_STORAGEADAPTER_DOCUMENTDB_CONNSTRING \
+    --name pcs-storage-adapter \
     azureiotpcs/pcs-storage-adapter-dotnet:testing
