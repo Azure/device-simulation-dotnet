bcc8c927fa0b87ec29c5d92388b38a0ab0574951
Stephen Pryor <stpryor@microsoft.com>
2018-12-17 15:44:45 -0800

    Create replay thread (#325)
    * Create empty replay thread
    
    * Remove connection using
    
    * Remove references to connections in the replay task
    
    * Update name of replay task test
    
    * Move const to config file

---
+++ SimulationAgent/SimulationThreads/DeviceReplayTask.cs
@@ -0,0 +1,74 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Azure.IoTSolutions.DeviceSimulation.Services.Concurrency;
+using Microsoft.Azure.IoTSolutions.DeviceSimulation.Services.Diagnostics;
+
+namespace Microsoft.Azure.IoTSolutions.DeviceSimulation.SimulationAgent.SimulationThreads
+{
+    public interface IDeviceReplayTask
+    {
+        Task RunAsync(
+            ConcurrentDictionary<string, ISimulationManager> simulationManagers,
+            CancellationToken runningToken
+        );
+    }
+
+    public class DeviceReplayTask : IDeviceReplayTask
+    {
+        // Global settings, not affected by hub SKU or simulation settings
+        private readonly IAppConcurrencyConfig appConcurrencyConfig;
+
+        private readonly ILogger log;
+
+        public DeviceReplayTask(
+            IAppConcurrencyConfig appConcurrencyConfig,
+            ILogger logger)
+        {
+            this.appConcurrencyConfig = appConcurrencyConfig;
+            this.log = logger;
+        }
+
+        public async Task RunAsync(
+            ConcurrentDictionary<string, ISimulationManager> simulationManagers,
+            CancellationToken runningToken)
+        {
+            var tasks = new List<Task>();
+
+            while (!runningToken.IsCancellationRequested)
+            {
+                // TODO: Do work
+                // foreach (var manager in simulationManagers)
+                // {
+                // }
+
+                var before = DateTimeOffset.UtcNow.ToUnixTimeMilliseconds();
+
+                // Wait for any pending tasks.
+                if (tasks.Count > 0)
+                {
+                    await Task.WhenAll(tasks);
+                    tasks.Clear();
+                }
+
+                var durationMsecs = DateTimeOffset.UtcNow.ToUnixTimeMilliseconds() - before;
+                this.log.Debug("Device-replay loop completed", () => new { durationMsecs });
+                this.SlowDownIfTooFast(durationMsecs, this.appConcurrencyConfig.MinDeviceReplayLoopDuration);
+            }
+        }
+
+        private void SlowDownIfTooFast(long duration, int min)
+        {
+            // Avoid sleeping for only one millisecond
+            if (duration >= min || min - duration <= 1) return;
+
+            var pauseMsecs = min - (int) duration;
+            this.log.Debug("Pausing device-replay thread", () => new { pauseMsecs });
+            Thread.Sleep(pauseMsecs);
+        }
+    }
+}
