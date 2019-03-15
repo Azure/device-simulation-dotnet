105e7b162b4fba3e112db9cc646bec1979345551
sushilraje <suraje@microsoft.com>
2019-03-14 18:14:03 -0700

    Keryvault integration into RM

--- scripts/docker/content/run.sh
+++ scripts/docker/content/run.sh
@@ -3,4 +3,7 @@
 
 cd /app/
 
+# Running in current shell
+. set_env.sh PCS_IOTHUB_CONNSTRING iotHubConnectionString PCS_STORAGEADAPTER_WEBSERVICE_URL storageAdapterWebServiceUrl
+
 cd webservice && dotnet Microsoft.Azure.IoTSolutions.DeviceSimulation.WebService.dll
