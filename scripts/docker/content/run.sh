1050299d7937f549b94d7d665c3cd23ac2d99a94
sushilraje <suraje@microsoft.com>
2019-03-28 15:06:02 -0700

    updates

--- scripts/docker/content/run.sh
+++ scripts/docker/content/run.sh
@@ -6,7 +6,7 @@ cd /app/
 echo "Setting environment variables."
 # Running in current shell
 
-. set_env.sh PCS_IOTHUB_CONNSTRING iotHubConnectionString PCS_STORAGEADAPTER_WEBSERVICE_URL storageAdapterWebServiceUrl PCS_WEBUI_AUTH_AAD_TENANT aadTenantId PCS_AUTH_AUDIENCE aadAppId PCS_AAD_SECRET aadAppSecret PCS_AAD_CLIENT_SP_ID aadAppId
+. set_env.sh PCS_IOTHUB_CONNSTRING iotHubConnectionString PCS_STORAGEADAPTER_WEBSERVICE_URL storageAdapterWebServiceUrl PCS_WEBUI_AUTH_AAD_TENANT aadTenantId PCS_AUTH_AUDIENCE aadAppId PCS_AAD_SECRET aadAppSecret PCS_AAD_CLIENT_SP_ID aadAppId PCS_AUTH_REQUIRED authRequired PCS_AUTH_ISSUER authIssuer
 
 echo "Starting service."
 cd webservice && dotnet Microsoft.Azure.IoTSolutions.DeviceSimulation.WebService.dll
